cc8597ac1350eff8fb99a84775d0491d2664e2d8
Daniel Molnar <dm@tpwd.de>
2022-02-11 20:27:59 +0100

    chore: wrap `navigation.setOptions` in `useLayoutEffect`

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -12,13 +12,15 @@ const BottomTab = createBottomTabNavigator();
 const INITIAL_ROUTE_NAME = 'Home';
 
 export default function BottomTabNavigator({ navigation, route }) {
-  // Set the header title on the parent stack navigator depending on the
-  // currently active tab. Learn more in the documentation:
-  // https://reactnavigation.org/docs/en/screen-options-resolution.html
-  navigation.setOptions({
-    headerTitle: getHeaderTitle(route),
-    headerRight: getHeaderRight(route)
-  });
+  React.useLayoutEffect(() => {
+    // Set the header title on the parent stack navigator depending on the
+    // currently active tab. Learn more in the documentation:
+    // https://reactnavigation.org/docs/en/screen-options-resolution.html
+    navigation.setOptions({
+      headerTitle: getHeaderTitle(route),
+      headerRight: getHeaderRight(route)
+    });
+  }, [navigation, route]);
 
   return (
     <BottomTab.Navigator initialRouteName={INITIAL_ROUTE_NAME}>
